406f4a1d5363c853a412e8075195df3ec2ba22ed
unknown <shir.armony@mail.huji.ac.il>
2024-01-08 22:14:18 +0200

    link maps commit

--- main.py
+++ main.py
@@ -139,7 +139,10 @@ def generate_itinerary(start_place, end_place, must_see, max_km, budget,
                      " 'https://www.google.com/maps/dir/'.")
     user_message += ("- Include the city names and their corresponding countries separated by '+' "
                      "for each destination within the link.")
-    user_message += "- Please REFRAIN from any unnecessary characters before or after the link."
+    user_message += ("- Please REFRAIN from any unnecessary characters before or after the link. "
+                     "So the link will be for instance:")
+    hyper_example = f'=HYPERLINK("https://www.google.com/maps/dir/Paris+France/Fontainebleau+France)"'
+    user_message += hyper_example
 
     st.write(user_message)
 
